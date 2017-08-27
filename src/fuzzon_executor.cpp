b161d48d4a446cb0998d20ced06655a7e06f1bff
Jacek Weremko <dablju@dablju-MS-7821>
2017-08-27 20:07:33 +0200

    Executor: SUT crash handling

--- src/fuzzon_executor.cpp
+++ src/fuzzon_executor.cpp
@@ -49,16 +49,16 @@ ExecutionData Executor::ExecuteBlocking(TestCase& input)
 		argv[i] = tokens[i].c_str();
 	}
 
-	Logger::Get()->debug(std::string("sut_path_: ") + sut_path_);
+//	Logger::Get()->debug(std::string("sut_path_: ") + sut_path_);
 	Logger::Get()->debug(std::string("argc: ") + std::to_string(argc) + std::string("  argv:") + input.string());
 //	Logger::Get()->debug(std::string("  argv : ") + argv_string);
 //	Logger::Get()->debug(std::string("  argv : ") + input.string());
 
 
-	Logger::Get()->debug("Execution begin");
-	boost::process::group group;
+//	Logger::Get()->debug("Execution begin");
+//	boost::process::group group;
 
-	boost::asio::io_service ios;
+//	boost::asio::io_service ios;
 //	std::future<std::string> sut_std_out;
 //	std::future<std::string> sut_std_err;
 //	boost::process::child sut(sut_path_, input.string(), group,
@@ -72,23 +72,19 @@ ExecutionData Executor::ExecuteBlocking(TestCase& input)
 	boost::process::ipstream sut_std_out_ips;
 	boost::process::ipstream sut_std_err_ips;
 
-	boost::process::child sut(sut_path_, input.string(), group,
+//	boost::process::child sut(sut_path_, input.string(), group,
+//			boost::process::std_out > sut_std_out_ips,
+//			boost::process::std_err > sut_std_err_ips);
+	boost::process::child sut(sut_path_, input.string(),
 			boost::process::std_out > sut_std_out_ips,
 			boost::process::std_err > sut_std_err_ips);
 
-
 	std::error_code ec = std::error_code(-999, std::system_category());
-//	if (!sut.wait_for(std::chrono::seconds(execution_timeout_sec_)))
-//	{
-//		sut.terminate(ec);
-//	}
-	if (!group.wait_for(std::chrono::seconds(execution_timeout_sec_)))
+	auto is_timeout_occure = sut.wait_for(std::chrono::seconds(execution_timeout_sec_), ec);
+	if (is_timeout_occure)
 	{
-		group.terminate(ec);
+		sut.terminate(ec);
 	}
-	sut.wait();
-	ec = std::error_code(sut.exit_code(), std::generic_category());
-
 
 	/* TODO: performance */
 	std::string line;
@@ -100,31 +96,19 @@ ExecutionData Executor::ExecuteBlocking(TestCase& input)
     while (std::getline(sut_std_err_ips, line))
     	sut_std_err << line << std::endl;
 
-	Logger::Get()->debug("Execution end");
+//	Logger::Get()->debug("Execution end");
 
 	delete [] argv;
 
 
-
-	Logger::Get()->debug(std::string("  ec: ") + std::to_string(ec.value()));
-	Logger::Get()->debug(std::string("  sut_std_out: ") + sut_std_out.str());
-	Logger::Get()->debug(std::string("  sut_std_err: ") + sut_std_err.str());
-
+//	Logger::Get()->debug(std::string("  ec: ") + std::to_string(ec.value()));
+//	Logger::Get()->debug(std::string("  ec: ") + std::string(ec.message()));
+//	Logger::Get()->debug(std::string("  sut_std_out: ") + sut_std_out.str());
+//	Logger::Get()->debug(std::string("  sut_std_err: ") + sut_std_err.str());
 	ExecutionTracker::Get()->GetCoverage()->PrintTrace();
 
-	try
-	{
 //	auto execution_output = std::make_unique<ExecutionData>(input, ec, sut_std_out, sut_std_err,*ExecutionTracker::Get()->GetCoverage());
-	ExecutionData execution_output(input, ec, sut_std_out, sut_std_err, ExecutionTracker::Get()->GetCoverage());
-	}
-	catch(std::runtime_error& ex)
-	{
-		std::cout << ec.message();
-	}
-
-	ExecutionData sdg(input, ec, sut_std_out, sut_std_err, ExecutionTracker::Get()->GetCoverage());
-	ExecutionTracker::Get()->GetCoverage()->PrintTrace();
-	return sdg;
+	return ExecutionData(input, ec, sut_std_out, sut_std_err, ExecutionTracker::Get()->GetCoverage());
 }
 
 } /* namespace fuzzon */
