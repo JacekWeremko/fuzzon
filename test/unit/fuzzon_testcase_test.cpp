ab6342c37b42789f23d1c684d9b24276d5a37a81
Jacek Weremko <dablju@dablju-MS-7821>
2017-09-02 15:59:59 +0200

    Remove obsolete UT
    Change-Id: I1d111ea5dee101d100f49e9655aab6195d4cc838
    Signed-off-by: Jacek Weremko <dablju@dablju-MS-7821>

--- test/unit/fuzzon_testcase_test.cpp
+++ test/unit/fuzzon_testcase_test.cpp
@@ -11,18 +11,18 @@
 
 BOOST_AUTO_TEST_SUITE(fuzzon_testcase_suite)
 
-void argc_test()
-{
-	std::string test_string = "should detect four words";
-	fuzzon::TestCase new_test_case(test_string);
-	auto argc = new_test_case.argc();
-	BOOST_TEST(argc == 4);
-}
-
-BOOST_AUTO_TEST_CASE(argc)
-{
-	argc_test();
-}
+//void argc_test()
+//{
+//	std::string test_string = "should detect four words";
+//	fuzzon::TestCase new_test_case(test_string);
+//	auto argc = new_test_case.argc();
+//	BOOST_TEST(argc == 4);
+//}
+//
+//BOOST_AUTO_TEST_CASE(argc)
+//{
+//	argc_test();
+//}
 
 BOOST_AUTO_TEST_SUITE_END()
 
